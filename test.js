6fce4a0a2c724f4d310b9130f8a658ee26e3bc96
charlesmonroeofficial <charlesmonroeofficial@gmail.com>
2017-03-23 15:50:57 +0200

    Add files via upload

--- test.js
+++ test.js
@@ -89,7 +89,8 @@
                         s(t, f), "function" == typeof m && m()
                         var n = window.URL || window.webkitURL,
                             r = n.createObjectURL(t);
-                        e.location.href = r, e.location.title = f
+                        e.location.href = r; 
+                        e.location.title = f;
                     }, t)
                 } catch (t) {
                     throw e.close(), t
